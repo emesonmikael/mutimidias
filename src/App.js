cc4eab23f10618e13473dde5b6b54ac2c2c064d4
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-12 11:13:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Eletro Expresso. teste
+          Edit <code>src/App.js</code> vamos
         </p>
         <a
           className="App-link"
