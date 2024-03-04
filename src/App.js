5b3bdec80b6914fcb4b7e859766090ad82d6a3a2
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-03-04 15:06:19 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {ethers} from 'ethers';
-import {useState} from 'react';
+//import {useLayoutEffect, useState} from 'react';
+//import {useState} from 'react';
+import { calcBNBPrice,calcBSTTPrice } from './bsttprice';
+
+//const [priceEle,setEle] = useState("");
+
+
 function App() {
+
+ async function elePrice(){
+  let bnbPrice = await calcBNBPrice() // query pancakeswap to get the price of BNB in USDT
+  let bsttPrice = await calcBSTTPrice() // query pancakeswap to get the price of BSTT in BNB
+  let bsttprice = bsttPrice * bnbPrice;
+ 
+  
+document.getElementById("bsttprice").innerHTML  = bsttprice.toFixed(10) ;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> vamos
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>oi</p>
+       <input type="button" value="COMPRAR" onClick={evt => elePrice()} />
+       <b id ="bsttprice">sdfs</b> 
       </header>
     </div>
   );
